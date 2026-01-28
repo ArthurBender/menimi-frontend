60ba15c2fac8bf7f8f741ee2eae4cbfee4f20a82
Arthur Bender <arthurllbender@gmail.com>
2026-01-28 14:46:09 -0300

    Feat: add react router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 
-function App() {
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Calendar from './pages/Calendar'
+import Home from './pages/Home'
+import NewTask from './pages/NewTask'
 
+function App() {
   return (
-    <div>Hello world!</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<NewTask />} />
+      <Route path="/calendar" element={<Calendar />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   )
 }
 
