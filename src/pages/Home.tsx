fb54aba6cd8740fc955982559149c6d3b3d5702e
Arthur Bender <arthurllbender@gmail.com>
2026-02-23 11:10:44 -0300

    Feat: add homescreen calendar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
 import { username, tasks, resume } from "../api/mock";
+
+import { Calendar, momentLocalizer } from "react-big-calendar"
 import HomeTaskRow from "../components/HomeTaskRow";
+
+import moment from "moment";
+
+const localizer = momentLocalizer(moment);
+
+const currentMonth = moment().format("MMMM - YYYY");
 
 const Home = () => {
   return (
@@ -7,7 +16,14 @@ const Home = () => {
       <div className="flex flex-col gap-10 justify-between h-full">
         <div className="flex gap-10 items-center justify-around">
           <h2 className="text-7xl font-semibold">Hey, there {username}!</h2>
-          <i className="h-50 w-50 bg-gray-300"></i>
+          <div className="h-50 w-100">
+            <p className="text-center font-medium">{currentMonth}</p>
+            <Calendar 
+              localizer={localizer}
+              views={["month"]}
+              toolbar={false}
+            />
+          </div>
         </div>
 
         <p className="text-xl bg-surface p-5 rounded-3xl">{resume}</p>
